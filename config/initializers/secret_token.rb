5f2aa67ceb8486a023b8463e9cc60e09abfe6253
Luan Haddad Ricardo dos Santos <luan@luansantos.com>
2011-09-23 12:02:55 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+C3slWeb::Application.config.secret_token = '27bdf577f13a06f04b8e6ae484607a856dc65ee63d50bb8a19d11c4f12bf0cd0a1f0a7327735340e5abe4f4886e728b8985d3062c65c4ce79813b45521fe9615'
